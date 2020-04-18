e492be51f46415126be77d6d9c6f47841c5e8bd5
pedrodalvy <pedrodalvy@outlook.com>
2020-04-17 21:01:38 -0400

    Configurado o favicon e brand icon

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -2,11 +2,11 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('admin.home') }}">
         <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
+            <img src="{{ asset('img/brand-icon.png') }}" alt="Brand incon" height="42" width="42">
         </div>
-        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+        <div class="sidebar-brand-text mx-2">Pedro Dalvy</div>
     </a>
 
     <!-- Divider -->
