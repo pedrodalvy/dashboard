dcdd5e64ae85b778c982dc7cd86f0e3f9732c5d8
pedrodalvy <pedrodalvy@outlook.com>
2020-04-29 15:56:07 -0400

    Alteração do layout da sidebar

--- resources/views/partials/admin/sidebar.blade.php
+++ resources/views/partials/admin/sidebar.blade.php
@@ -26,68 +26,31 @@
 
     <!-- Heading -->
     <div class="sidebar-heading">
-        Cadastros
+        Gestão do site
     </div>
 
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item {{ \Request::is('admin/users*') ? 'active' : '' }}">
-
-        <a class="nav-link {{ \Request::is('admin/users/*') ? '' : 'collapsed' }}" href="#" data-toggle="collapse"
-            data-target="#collapseUsers" aria-expanded="true" aria-controls="collapseUsers">
-            <i class="fas fa-users"></i>
-            <span>Usuários</span>
+    <li class="nav-item {{ \Request::is('admin/resume/profile*') ? 'active' : '' }}">
+        <a class="nav-link load" href="{{ route('resume.profile') }}">
+            <i class="fas fa-id-badge"></i>
+            <span>Perfil</span>
         </a>
+    </li>
 
-        <div id="collapseUsers" class="collapse {{ \Request::is('admin/users*') ? 'show' : '' }}"
-            aria-labelledby="headingUsers" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
+    <li class="nav-item {{ \Request::is('admin/resume/experience*') ? 'active' : '' }}">
+        <a class="nav-link load" href="{{ route('experience.index') }}">
+            <i class="fas fa-briefcase"></i>
+            <span>Experiência</span>
+        </a>
+    </li>
 
-                <h6 class="collapse-header">Cadastro de usuários:</h6>
-
-                <a class="collapse-item load {{ \Request::is('admin/users*') ? 'active' : '' }}"
-                    href="{{ route('admin.users') }}">
-                    Todos
-                </a>
-
-                <a class="collapse-item load" href="#">
-                    Novo
-                </a>
-            </div>
-        </div>
+    <li class="nav-item {{ \Request::is('admin/resume/education*') ? 'active' : '' }}">
+        <a class="nav-link load" href="{{ route('education.index') }}">
+            <i class="fas fa-graduation-cap"></i>
+            <span>Formação</span>
+        </a>
     </li>
 
 
-    <li class="nav-item {{ \Request::is('admin/resume/*') ? 'active' : '' }}">
-
-        <a class="nav-link {{ \Request::is('admin/resume/*') ? '' : 'collapsed' }}" href="#" data-toggle="collapse"
-            data-target="#collapseResume" aria-expanded="true" aria-controls="collapseResume">
-            <i class="fas fa-file-alt"></i>
-            <span>Currículo</span>
-        </a>
-
-        <div id="collapseResume" class="collapse {{ \Request::is('admin/resume/*') ? 'show' : '' }}"
-            aria-labelledby="headingResume" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Manutenção do Currículo:</h6>
-
-                <a class="collapse-item load {{ \Request::is('admin/resume/profile*') ? 'active' : '' }}"
-                    href="{{ route('resume.profile') }}">
-                    Perfil
-                </a>
-
-                <a class="collapse-item load {{ \Request::is('admin/resume/experience*') ? 'active' : '' }}"
-                    href="{{ route('experience.index') }}">
-                    Experiência
-                </a>
-
-                <a class="collapse-item load {{ \Request::is('admin/resume/education*') ? 'active' : '' }}"
-                    href="{{ route('education.index') }}">
-                    Formação
-                </a>
-
-            </div>
-        </div>
-    </li>
 
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
