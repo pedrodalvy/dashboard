467cf7a9e3b05b19fff17eccd68db1039f206b36
pedrodalvy <pedrodalvy@outlook.com>
2020-04-29 13:45:45 -0400

    Modificação na factory do profile

--- database/factories/ResumeProfileFactory.php
+++ database/factories/ResumeProfileFactory.php
@@ -7,12 +7,13 @@ use Faker\Generator as Faker;
 
 $factory->define(ResumeProfile::class, function (Faker $faker) {
     return [
-        'name' => 'Pedro Felipe Dalvy Ramires',
-        'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus suscipit accusamus vitae impedit pariatur, quas, provident fugit ducimus eligendi labore aspernatur natus porro aperiam illo officia obcaecati corporis nisi molestias.',
-        'email' => 'pedrodalvy@outlook.com',
+        'name' => $faker->name(),
+        'description' => $faker->text($maxNbChars = 300),
+        'email' => $faker->email(),
         'fone' => '(99) 9 9999-9999',
         'address' => 'Centro, Campo Grande/MS - Brasil',
         'function' => 'Desenvolvedor Full-Stack',
         'pricing' => 3000.00,
+        'skills' => 'PHP,HTML,CSS,JavaScript',
     ];
 });
