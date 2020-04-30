632376963c13c913354795dfa58dcea7f2d4f88a
pedrodalvy <pedrodalvy@outlook.com>
2020-04-30 10:24:11 -0400

    Correção no layout da sidebar

--- resources/views/partials/admin/sidebar.blade.php
+++ resources/views/partials/admin/sidebar.blade.php
@@ -14,7 +14,7 @@
     <hr class="sidebar-divider my-0">
 
     <!-- Nav Item - Dashboard -->
-    <li class="nav-item {{ \Request::is('admin') ? 'active' : '' }}">
+    <li class="nav-item {{ \Request::is('/') ? 'active' : '' }}">
         <a class="nav-link load" href="{{ route('admin.home') }}">
             <i class="fas fa-home"></i>
             <span>Home</span>
@@ -29,21 +29,21 @@
         Gestão do site
     </div>
 
-    <li class="nav-item {{ \Request::is('admin/resume/profile*') ? 'active' : '' }}">
+    <li class="nav-item {{ \Request::is('resume/profile*') ? 'active' : '' }}">
         <a class="nav-link load" href="{{ route('resume.profile') }}">
             <i class="fas fa-id-badge"></i>
             <span>Perfil</span>
         </a>
     </li>
 
-    <li class="nav-item {{ \Request::is('admin/resume/experience*') ? 'active' : '' }}">
+    <li class="nav-item {{ \Request::is('resume/experience*') ? 'active' : '' }}">
         <a class="nav-link load" href="{{ route('experience.index') }}">
             <i class="fas fa-briefcase"></i>
             <span>Experiência</span>
         </a>
     </li>
 
-    <li class="nav-item {{ \Request::is('admin/resume/education*') ? 'active' : '' }}">
+    <li class="nav-item {{ \Request::is('resume/education*') ? 'active' : '' }}">
         <a class="nav-link load" href="{{ route('education.index') }}">
             <i class="fas fa-graduation-cap"></i>
             <span>Formação</span>
